9094e4b68d31adba8fd9f706e237abcb20d3b077
Yunlongl <yunlongl@sfu.ca>
2021-06-03 00:21:16 +0800

    initial

---
+++ list.c
@@ -0,0 +1,65 @@
+#include "list.h"
+
+List* List_create() {
+    
+}
+
+int List_count(List* pList) {
+    // go
+}
+
+void* List_first(List* pList) {
+
+}
+
+void* List_last(List* pList) {
+
+}
+
+void* List_next(List* pList) {
+
+}
+
+void* List_prev(List* pList) {
+
+}
+
+void* List_curr(List* pList) {
+
+}
+
+int List_add(List* pList, void* pItem) {
+
+}
+
+int List_insert(List* pList, void* pItem) {
+
+}
+
+int List_append(List* pList, void* pItem) {
+
+}
+
+int List_prepend(List* pList, void* pItem) {
+
+}
+
+void* List_remove(List* pList) {
+
+}
+
+void List_concat(List* pList1, List* pList2) {
+
+}
+
+void List_free(List* pList, FREE_FN pItemFreeFn) {
+
+}
+
+void* List_trim(List* pList) {
+
+}
+
+void* List_search(List* pList, COMPARATOR_FN pComparator, void* pComparisonArg) {
+
+}
